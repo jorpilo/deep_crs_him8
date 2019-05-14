8bb37a029d8fad02fe54fc38f8d789a0c1cb3696
mio7c7 <44826831+mio7c7@users.noreply.github.com>
2019-05-14 17:13:50 +1000

    Update optical_flow_all_pixels.py

--- cloud_optical_flow/optical_flow_all_pixels.py
+++ cloud_optical_flow/optical_flow_all_pixels.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue May 14 15:09:12 2019
 
 @author: RMC
 """
-
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 import xarray as xr
-import os
+
 
 # The video feed is read in as a VideoCapture object
 cap = xr.open_dataset("D:/dataset/H8_Flow.nc")
 
 a = np.asarray(cap['B7'])
-lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+lkParams=dict( winSize =(15,15),
+                  maxLevel=2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 dataset = np.round(255*(a - np.min(a))/np.ptp(a).astype(int)).astype(np.uint8)
 
 gray = base = dataset[0,:,:]
+height, width = gray.shape
 
 # ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
 cv2.imshow('frame',gray)
@@ -31,16 +31,33 @@ for i in range(1, len(dataset)):
     all_pixels = np.nonzero(frameGray)[::-1]
     all_pixels = tuple(zip(*all_pixels))
     all_pixels = np.vstack(all_pixels).reshape(-1, 1, 2).astype("float32")
-    
-    p1, st, err = cv2.calcOpticalFlowFarneback(gray, frameGray, all_pixels, None, **lk_params)
-    #all_pixels = all_pixels.reshape(height, width, 2)
-    #p1 = p1.reshape(height, width, 2)
+       
+    p1, st, err = cv2.calcOpticalFlowPyrLK(gray, frameGray, all_pixels, None, **lkParams)
+    all_pixels = all_pixels.reshape(height, width, 2)
+    p1 = p1.reshape(height, width, 2)
     
     #img = cv2.add(frame,mask)
+
+    # Flow vector calculated by subtracting new pixels by old pixels
+    flow = p1 - all_pixels
+    step=8
+    
+    h, w = frameGray.shape[:2]
+    y, x = np.mgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
+    fx, fy = flow[y, x].T
+    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    cv2.polylines(frameGray, lines, 0, (0, 255, 0))
+    for (x1, y1), (_, _) in lines:
+        frame = cv2.circle(frameGray, (x1, y1), 1, (0, 255, 0), -1)
+        
+    img = cv2.add(frame,mask)
+
+    cv2.imshow('frame',img)
+    cv2.waitKey()
     
     gray = frameGray
     mask = np.zeros_like(gray)
 
-
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
