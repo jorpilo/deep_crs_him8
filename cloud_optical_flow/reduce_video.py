43be6c4e4233fff0046444c5d4e637b16352eca1
Jorge Pinilla López <jorpilo@gmail.com>
2019-05-19 17:01:01 +1000

    added reduced dataset

--- cloud_optical_flow/reduce_video.py
+++ cloud_optical_flow/reduce_video.py
@@ -4,14 +4,21 @@ import matplotlib.animation as animation
 import xarray as xr
 
 
-fig = plt.figure()
-ds = xr.open_dataset("../../dataset/sat_pre_video2/sortH8_2017.nc")
+#fig = plt.figure()
+ds = xr.open_dataset("../../dataset/HIM8_2017.nc")
 
-ds
 #ds = xr.open_dataset("sort.nc")
 #ds
 
+ds = ds.drop(['B8','B10', 'B12', 'B13', 'B14','B15'])
+print(ds)
 
+comp = dict(zlib=True, complevel=9)
+encoding = {var: comp for var in ds.data_vars}
+
+
+ds.to_netcdf('sort.nc',mode='w', format='NETCDF4', engine='h5netcdf',encoding=encoding)
+"""
 ds
 print(ds.time)
 #print(ds["B7"][:,:,:].min())
@@ -19,11 +26,11 @@ print(ds.time)
 #sys.exit()
 
 ims = []
-for i in range(100):
+for i in range(len(ds["B7"])):
     im = plt.imshow(ds["B7"][i,:,:], animated=True)
     ims.append([im])
-    print(i)
 print(len(ims))
 ani = animation.ArtistAnimation(fig, ims, interval=200, blit=True, repeat_delay=1000)
 
 ani.save('cloudsB7.mp4')
+"""
